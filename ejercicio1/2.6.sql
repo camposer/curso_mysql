0a2c0fe8e0b3c2793904839560eefd741d5a0d73
Rodolfo Campos <camposer@gmail.com>
2015-11-05 11:42:46 +0100

    Solución del ejercicio 2.6

---
+++ ejercicio1/2.6.sql
@@ -0,0 +1,15 @@
+SELECT
+	p.*, a.*, COUNT(p.id) numero_compras
+FROM compra c
+INNER JOIN libro l ON l.id = c.libro_id
+INNER JOIN autor a ON a.id = l.autor_id
+INNER JOIN persona p ON p.id = a.id
+ORDER BY numero_compras DESC
+LIMIT 1;
+
+SELECT
+        p.*, a.*, COUNT(p.id) numero_compras
+FROM compra c, libro l, autor a, persona p
+WHERE l.id = c.libro_id AND a.id = l.autor_id AND p.id = a.id
+ORDER BY numero_compras DESC
+LIMIT 1;
